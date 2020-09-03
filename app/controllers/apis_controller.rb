90ba98be6499852a706ade5a44a16cc68ef9ff0c
apple-yagi <applesryu@gmail.com>
2020-09-03 11:55:48 +0900

    index, show, new, create機能を実装

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,2 +1,30 @@
 class ApisController < ApplicationController
+  def index
+    @apis = Api.all
+  end
+
+  def show
+  end
+
+  def new
+    @createApi = Api.new
+  end
+
+  def create
+    @api = Api.create(api_params)
+    if @api.save
+      redirect_to apis_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def api_params
+    params.require(:api).permit(:domain, :url, :description)
+  end
+
+  def set_api
+    @api = Api.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'qiita#index'
-  resources :qiita, only: [:index]
+  root to: 'apis#index'
+  resources :apis, only: [:index, :new, :show, :create]
 end

e9fab3e0c3a0742acc8e2d9cb3aa1c7be6096f2c
apple-yagi <applesryu@gmail.com>
2020-09-03 09:52:03 +0900

    qiita_controllerを削除

--- app/controllers/qiita_controller.rb
+++
@@ -1,8 +0,0 @@
-require_relative './concerns/client'
-
-class QiitaController < ApplicationController
-  def index
-    c = Client.new("https://qiita.com/api/v2/items?page=1&per_page=20")
-    @data = c.fetch_data
-  end
-end
